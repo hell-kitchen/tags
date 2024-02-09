93e764833cb64408935bf7429738fa338268804a
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-09 21:59:10 +0300

    added fx integration

--- cmd/tags/tags.go
+++ cmd/tags/tags.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+)
 
+func main() {
+	fx.New(NewOptions()).Run()
+}
+
+func NewOptions() fx.Option {
+	return fx.Options(
+		fx.Provide(
+			zap.NewProduction,
+		),
+		fx.Invoke(),
+	)
 }
