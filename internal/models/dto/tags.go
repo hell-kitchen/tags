2af835b16d2047371b58975700830ea510559cb5
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-15 23:15:15 +0300

    added tags dto

---
+++ internal/models/dto/tags.go
@@ -0,0 +1,15 @@
+package dto
+
+type (
+	TagCreationDTO struct {
+		Name  string
+		Color string
+		Slug  string
+	}
+	TagDTO struct {
+		ID    string
+		Name  string
+		Color string
+		Slug  string
+	}
+)
