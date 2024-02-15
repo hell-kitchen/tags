34e6cae210d9e4a297de4c4aff605943e8a3ccdc
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-15 22:31:17 +0300

    added creation of Config via confita

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,20 @@
 package config
 
+import (
+	"context"
+	"github.com/hell-kitchen/pkg/confita"
+)
+
 type Config struct {
 	BindPort int    `config:"bind-port"`
 	BindHost string `config:"bind-host"`
 	BaseAddr string `config:"base-addr"`
+}
+
+func New() (*Config, error) {
+	var cfg Config
+	if err := confita.Get().Load(context.Background(), &cfg); err != nil {
+		return nil, err
+	}
+	return &cfg, nil
 }
