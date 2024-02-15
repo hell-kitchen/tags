6c7fb0f8c18453b8e54c0bc2ca890e9254ddd35b
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-15 22:56:38 +0300

    added TLS configuration

--- internal/config/config.go
+++ internal/config/config.go
@@ -2,19 +2,35 @@ package config
 
 import (
 	"context"
+	"fmt"
 	"github.com/hell-kitchen/pkg/confita"
 )
 
 type Config struct {
-	BindPort int    `config:"bind-port"`
-	BindHost string `config:"bind-host"`
-	BaseAddr string `config:"base-addr"`
+	BindPort int    `config:"bind-port,short=p"`
+	BindHost string `config:"bind-host,short=h"`
+	BaseAddr string `config:"base-addr,short=a"`
+	UseTLS   bool   `config:"use-tls"`
+	CertFile string `config:"cert-file"`
+	KeyFile  string `config:"key-file"`
 }
 
+// New initializes new server config.
 func New() (*Config, error) {
-	var cfg Config
-	if err := confita.Get().Load(context.Background(), &cfg); err != nil {
+	cfg := &Config{
+		BindPort: 8080,
+		BindHost: "0.0.0.0",
+		BaseAddr: "http://localhost:8080",
+		UseTLS:   false,
+		CertFile: "",
+		KeyFile:  "",
+	}
+	if err := confita.Get().Load(context.Background(), cfg); err != nil {
 		return nil, err
 	}
-	return &cfg, nil
+	return cfg, nil
+}
+
+func (cfg Config) Bind() string {
+	return fmt.Sprintf("%s:%d", cfg.BindHost, cfg.BindPort)
 }
