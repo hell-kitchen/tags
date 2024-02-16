5a1995d4582d4ea3b2de4b0f4398df7756fd9a95
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-16 21:43:48 +0300

    implemented service create method

--- internal/service/production/handlers.go
+++ internal/service/production/handlers.go
@@ -2,12 +2,23 @@ package production
 
 import (
 	"context"
+	"github.com/google/uuid"
 	"github.com/hell-kitchen/tags/internal/models/dto"
 )
 
-func (s *Service) Create(ctx context.Context, dto dto.TagCreationDTO) (*dto.TagDTO, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *Service) Create(ctx context.Context, creationData dto.TagCreationDTO) (*dto.TagDTO, error) {
+	tag := &dto.TagDTO{
+		ID:    uuid.New(),
+		Name:  creationData.Name,
+		Color: creationData.Color,
+		Slug:  creationData.Slug,
+	}
+
+	if err := s.repository.Create(ctx, tag); err != nil {
+		return nil, err
+	}
+
+	return tag, nil
 }
 
 func (s *Service) Get(ctx context.Context, id string) (*dto.TagDTO, error) {
