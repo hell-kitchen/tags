29282c7006c973543e445e959f032f13295139d4
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-16 21:41:01 +0300

    added repository to service layer

--- internal/service/production/service.go
+++ internal/service/production/service.go
@@ -1,6 +1,7 @@
 package production
 
 import (
+	"github.com/hell-kitchen/tags/internal/repository"
 	"github.com/hell-kitchen/tags/internal/service"
 	"go.uber.org/zap"
 )
@@ -8,7 +9,8 @@ import (
 var _ service.TagsService = (*Service)(nil)
 
 type Service struct {
-	logger *zap.Logger
+	logger     *zap.Logger
+	repository repository.Interface
 }
 
 func New(logger *zap.Logger) (*Service, error) {
