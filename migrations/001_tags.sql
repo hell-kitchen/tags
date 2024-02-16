8293da1ef674aad9d75c2e112bbc8eb5b8920be4
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-16 22:29:25 +0300

    fixed database scheme

--- migrations/001_tags.sql
+++ migrations/001_tags.sql
@@ -1,8 +1,9 @@
-CREATE TABLE tags (
-    "id" UUID PRIMARY KEY NOT NULL UNIQUE,
-    "name" VARCHAR(255) NOT NULL,
-    "slug" VARCHAR(255) NOT NULL,
-    "color" VARCHAR(255) NOT NULL
+CREATE TABLE tags
+(
+    "id"    UUID PRIMARY KEY NOT NULL UNIQUE,
+    "name"  VARCHAR(255)     NOT NULL UNIQUE,
+    "slug"  VARCHAR(255)     NOT NULL UNIQUE,
+    "color" VARCHAR(255)     NOT NULL UNIQUE
 );
 
 ---- create above / drop below ----
