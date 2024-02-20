8d26b4c4d9cebf37caa722e83225991c13bf7edb
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-20 13:11:44 +0300

    implemented unimplemented methods

--- internal/controller/grpc/handlers.go
+++ internal/controller/grpc/handlers.go
@@ -2,16 +2,17 @@ package grpc
 
 import (
 	"context"
-	"github.com/hell-kitchen/tags/internal/models/dto/convertor"
+	old "github.com/hell-kitchen/tags/internal/models/dto/convertor"
+	"github.com/hell-kitchen/tags/internal/pkg/convertor"
 	pb "github.com/hell-kitchen/tags/pkg/api/proto"
 )
 
 func (ctrl *Controller) Create(ctx context.Context, req *pb.CreateRequest) (*pb.CreateResponse, error) {
-	resp, err := ctrl.service.Create(ctx, convertor.FromTagCreateRequest(req))
+	resp, err := ctrl.service.Create(ctx, old.FromTagCreateRequest(req))
 	if err != nil {
 		return nil, err
 	}
-	return convertor.ToTagsCreateResponse(resp), nil
+	return convertor.FromTagDTO(*resp).ToCreateResponse(), nil
 }
 
 func (ctrl *Controller) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetResponse, error) {
@@ -19,7 +20,7 @@ func (ctrl *Controller) Get(ctx context.Context, req *pb.GetRequest) (*pb.GetRes
 	if err != nil {
 		return nil, err
 	}
-	return convertor.ToTagsGetResponse(resp), nil
+	return convertor.FromTagDTO(*resp).ToGetResponse(), nil
 }
 
 func (ctrl *Controller) GetAll(ctx context.Context, _ *pb.GetAllRequest) (*pb.GetAllResponse, error) {
@@ -27,16 +28,29 @@ func (ctrl *Controller) GetAll(ctx context.Context, _ *pb.GetAllRequest) (*pb.Ge
 	if err != nil {
 		return nil, err
 	}
-	return convertor.ToTagsGetAllResponse(resp), nil
+	return old.ToTagsGetAllResponse(resp), nil
 }
 
-func (ctrl *Controller) CreateMany(context.Context, *pb.CreateManyRequest) (*pb.CreateManyResponse, error) {
-	panic("not implemented")
+func (ctrl *Controller) CreateMany(ctx context.Context, req *pb.CreateManyRequest) (*pb.CreateManyResponse, error) {
+	resp, err := ctrl.service.CreateMany(ctx, old.FromTagCreateManyRequest(req))
+	if err != nil {
+		return nil, err
+	}
+	return old.ToTagsCreateManyResponse(resp), nil
 }
 
-func (ctrl *Controller) Delete(context.Context, *pb.DeleteRequest) (*pb.DeleteResponse, error) {
-	panic("not implemented")
+func (ctrl *Controller) Delete(ctx context.Context, req *pb.DeleteRequest) (*pb.DeleteResponse, error) {
+	err := ctrl.service.Delete(ctx, req.GetId())
+	if err != nil {
+		return nil, err
+	}
+	return &pb.DeleteResponse{}, nil
 }
-func (ctrl *Controller) Update(context.Context, *pb.UpdateRequest) (*pb.UpdateResponse, error) {
-	panic("not implemented")
+
+func (ctrl *Controller) Update(ctx context.Context, req *pb.UpdateRequest) (*pb.UpdateResponse, error) {
+	resp, err := ctrl.service.Update(ctx, old.ProtoToUpdateDTO(req))
+	if err != nil {
+		return nil, err
+	}
+	return convertor.FromTagDTO(*resp).ToProtoUpdateResponse(), nil
 }
