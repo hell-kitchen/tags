58e4dd6c4d34a8c744191dc481ce33f0cf69b131
Vlad Marlo <vlad.marlo@ya.ru>
2024-02-24 23:20:18 +0300

    changed grpc server creation logic
    Added grpc interceptors;
    Changed credentials creation logic

--- go.mod
+++ go.mod
@@ -4,18 +4,22 @@ go 1.20
 
 require (
 	github.com/google/uuid v1.6.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
 	github.com/hell-kitchen/pkg/confita v0.0.0-20240216101713-dd9a826b0234
-	github.com/hell-kitchen/pkg/logger v0.0.0-20240216192317-c1d6492ee1bd
+	github.com/hell-kitchen/pkg/grpcmw v0.0.0-20240224192358-2314a7498685
+	github.com/hell-kitchen/pkg/logger v0.0.0-20240221165412-55c3ffe0b26d
 	github.com/hell-kitchen/pkg/postgres v0.0.0-20240216193337-3b3326857bf4
 	github.com/jackc/pgx/v5 v5.5.3
+	github.com/stretchr/testify v1.8.4
 	go.uber.org/fx v1.20.1
 	go.uber.org/multierr v1.11.0
-	go.uber.org/zap v1.26.0
-	google.golang.org/grpc v1.61.1
+	go.uber.org/zap v1.27.0
+	google.golang.org/grpc v1.62.0
 	google.golang.org/protobuf v1.32.0
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
 	github.com/heetch/confita v0.10.0 // indirect
 	github.com/hell-kitchen/pkg/retry v0.0.0-20240216101713-dd9a826b0234 // indirect
@@ -24,12 +28,14 @@ require (
 	github.com/jackc/pgx-zap v0.0.0-20221202020421-94b1cb2f889f // indirect
 	github.com/jackc/puddle/v2 v2.2.1 // indirect
 	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/vgarvardt/pgx-google-uuid/v5 v5.0.0 // indirect
 	go.uber.org/dig v1.17.1 // indirect
 	golang.org/x/crypto v0.19.0 // indirect
-	golang.org/x/net v0.18.0 // indirect
+	golang.org/x/net v0.21.0 // indirect
 	golang.org/x/sync v0.6.0 // indirect
 	golang.org/x/sys v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240213162025-012b6fc9bca9 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/controller/grpc/grpc.go
+++ internal/controller/grpc/grpc.go
@@ -2,33 +2,43 @@ package grpc
 
 import (
 	"context"
-	"errors"
-	"fmt"
+	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_recovery "github.com/grpc-ecosystem/go-grpc-middleware/recovery"
+	"github.com/hell-kitchen/pkg/grpcmw"
+	"github.com/hell-kitchen/pkg/logger"
 	"github.com/hell-kitchen/tags/internal/config"
 	"github.com/hell-kitchen/tags/internal/service"
 	pb "github.com/hell-kitchen/tags/pkg/api/proto"
 	"go.uber.org/multierr"
+	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
+	"google.golang.org/grpc/credentials/insecure"
 	"net"
 	"time"
 )
 
 type Controller struct {
 	pb.UnimplementedTagsServiceServer
+	log      *zap.Logger
 	server   *grpc.Server
 	cfg      *config.Controller
 	listener net.Listener
 	service  service.TagsService
 }
 
-func New(cfg *config.Controller, service service.TagsService) (ctrl *Controller, err error) {
+func New(cfg *config.Controller, service service.TagsService, log *zap.Logger) (ctrl *Controller, err error) {
+	ctrlLogger := log.With(
+		logger.WithLayer("controller"),
+		zap.String("bind-addr", cfg.Bind()),
+	)
 	ctrl = &Controller{
 		cfg:     cfg,
+		log:     ctrlLogger,
 		service: service,
 	}
 	multierr.AppendInto(&err, ctrl.createListener())
-	multierr.AppendInto(&err, ctrl.createServer())
+	multierr.AppendInto(&err, ctrl.createServer(log))
 	return ctrl, err
 }
 
@@ -37,36 +47,51 @@ func (ctrl *Controller) createListener() (err error) {
 	if err != nil {
 		return err
 	}
+	ctrl.log.Info("created listener")
 	return nil
 }
 
-func (ctrl *Controller) createServer() (err error) {
-	var opts []grpc.ServerOption
+func transportCredentialsByConfig(cfg *config.Controller) credentials.TransportCredentials {
+	if !cfg.UseTLS {
+		return insecure.NewCredentials()
+	}
 
-	if ctrl.cfg.UseTLS {
-		var transportCredentials credentials.TransportCredentials
+	if cfg.CertFile == "" || cfg.KeyFile == "" {
+		return insecure.NewCredentials()
+	}
 
-		if ctrl.cfg.CertFile == "" || ctrl.cfg.KeyFile == "" {
-			return errors.New("bad config")
-		}
+	creds, err := credentials.NewServerTLSFromFile(cfg.CertFile, cfg.KeyFile)
+	if err != nil {
+		return insecure.NewCredentials()
+	}
 
-		transportCredentials, err = credentials.NewServerTLSFromFile(ctrl.cfg.CertFile, ctrl.cfg.KeyFile)
-		if err != nil {
-			return fmt.Errorf("failed to generate credentials: %w", err)
-		}
+	return creds
+}
 
-		opts = []grpc.ServerOption{
-			grpc.Creds(transportCredentials),
-		}
+func (ctrl *Controller) createServer(log *zap.Logger) (err error) {
+	grpcLogger := log.With(logger.WithLayer("grpc"))
+
+	grpc_zap.ReplaceGrpcLoggerV2(grpcLogger)
+
+	ctrl.log.Info("creating gRPC server")
+	var opts = []grpc.ServerOption{
+		grpc.Creds(transportCredentialsByConfig(ctrl.cfg)),
+		grpc.ChainUnaryInterceptor(
+			grpc_zap.UnaryServerInterceptor(grpcLogger),
+			grpc_recovery.UnaryServerInterceptor(),
+			grpcmw.UnaryServerInterceptor(),
+		),
 	}
 
 	ctrl.server = grpc.NewServer(opts...)
 	pb.RegisterTagsServiceServer(ctrl.server, ctrl)
+	ctrl.log.Info("successfully created gRPC server")
 	return nil
 }
 
 func (ctrl *Controller) Start(ctx context.Context) error {
 	var cancel context.CancelCauseFunc
+	ctrl.log.Info("Start just called")
 
 	ctx, cancel = context.WithCancelCause(ctx)
 
